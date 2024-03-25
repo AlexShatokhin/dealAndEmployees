bc1f501285ba2d2e91d5a21f0f9b4358ffb0ece7
Alex Shatokhin <shatokhin.04@inbox.ru>
2024-03-25 09:33:37 +0200

    add redux to tasks list

--- src/components/EmployeeComponents/CompleteTask/CompleteTask.js
+++ src/components/EmployeeComponents/CompleteTask/CompleteTask.js
@@ -1,19 +1,22 @@
 import { Button } from "react-bootstrap"
+import { useDispatch } from "react-redux"
 
+import { toggleTaskAdded } from "../EmployeeSlice";
 import useModal from "../../../hooks/useModal"
 import useData from "../../../services/getData"
 
 import TaskInformation from "../../TaskInformation/TaskInformation"
 
-const CompleteTask = ({changeTaskAdded, employee, deal}) => {
+const CompleteTask = ({employee, deal}) => {
 
+    const dispatch = useDispatch();
     const {isShowModal, toggleModal, Modal} = useModal();
     const {editDeal} = useData();
 
 
     function changeTaskEmployee(){
         editDeal({status: "complete", employeeID: employee[0].id}, deal.taskID)
-        .then(changeTaskAdded)
+        .then(() => dispatch(toggleTaskAdded()))
         
     }
 
@@ -26,7 +29,12 @@ const CompleteTask = ({changeTaskAdded, employee, deal}) => {
         <div style={{width: 250, display: "flex", justifyContent: "space-between"}} className="btns_wrapper">
             {content}
             <Button onClick={toggleModal} variant="primary">Подробнее</Button>
-            <Button onClick={changeTaskEmployee} variant={deal.status == "complete" ? "success" : "primary"} disabled = {deal.status == "complete"} >{!deal.status == "complete" ? "Выполнить!" : "Выполнено!"}</Button>
+            <Button 
+                onClick={changeTaskEmployee} 
+                variant={deal.status === "complete" ? "success" : "primary"} 
+                disabled = {deal.status === "complete"} >
+                {!deal.status === "complete" ? "Выполнить!" : "Выполнено!"}
+            </Button>
         </div>
     )
 }
