c2213ad88bfb919fa19961db9a5693649f7054ee
Alex Shatokhin <shatokhin.04@inbox.ru>
2025-12-15 21:54:23 +0200

    add new variables

--- src/services/getData.js
+++ src/services/getData.js
@@ -1,73 +1,94 @@
 import useHttp from "../hooks/HttpHook"
 
+const HOST = "https://admin-panel-25m8.onrender.com"
+const PORT = 3030
+
 const useData = () => {
-    const {httpRequest, loading, error} = useHttp();
+	const { httpRequest, loading, error } = useHttp()
 
-    const getEmployee = async (id) => {
-        const employees = await httpRequest(`http://localhost:${3000}/employees/${id}`);
-        return employees;
-    }
+	const getEmployee = async (id) => {
+		const employees = await httpRequest(`${HOST}:${PORT}/employees/${id}`)
+		return employees
+	}
 
-    const getEmployees = async () => {
-        const employees = await httpRequest(`http://localhost:${3000}/employees`);
-        return employees;
-    }
+	const getEmployees = async () => {
+		const employees = await httpRequest(`http://${HOST}:${PORT}/employees`)
+		return employees
+	}
 
-    const setEmployees = async (body) => {
-        return await httpRequest(`http://localhost:${3000}/employees`, "POST", JSON.stringify(body));
-    }
+	const setEmployees = async (body) => {
+		return await httpRequest(
+			`${HOST}:${PORT}/employees`,
+			"POST",
+			JSON.stringify(body)
+		)
+	}
 
-    const editEmployee = async (body, id) => {
-        return await httpRequest(`http://localhost:${3000}/employees/${id}`, "PUT", JSON.stringify(body))
-    }
+	const editEmployee = async (body, id) => {
+		return await httpRequest(
+			`${HOST}:${PORT}/employees/${id}`,
+			"PUT",
+			JSON.stringify(body)
+		)
+	}
 
-    const deleteEmployees = async (id) => {
-        return await httpRequest(`http://localhost:${3000}/employees/${id}`, "DELETE");
-    }
+	const deleteEmployees = async (id) => {
+		return await httpRequest(`${HOST}:${PORT}/employees/${id}`, "DELETE")
+	}
 
-    const getDeals = async () => {
-        const deals = await httpRequest(`http://localhost:${3000}/deals`);
-        return deals;
-    }
+	const getDeals = async () => {
+		const deals = await httpRequest(`${HOST}:${PORT}/deals`)
+		return deals
+	}
 
-    const getDeal = async (id) => {
-        const deals = await httpRequest(`http://localhost:${3000}/deals/${id}`);
-        return deals;
-    }
+	const getDeal = async (id) => {
+		const deals = await httpRequest(`${HOST}:${PORT}/deals/${id}`)
+		return deals
+	}
 
-    const setDeal = async (body) => {
-        const dataToSend = {
-            ...body, employee: "nobody", status: "new"
-        }
-        return await httpRequest(`http://localhost:${3000}/deals/`, "POST", JSON.stringify(dataToSend));
-    }
+	const setDeal = async (body) => {
+		const dataToSend = {
+			...body,
+			employee: "nobody",
+			status: "new",
+		}
+		return await httpRequest(
+			`${HOST}:${PORT}/deals/`,
+			"POST",
+			JSON.stringify(dataToSend)
+		)
+	}
 
-    const editDeal = async (body, id) => {
-        return await httpRequest(`http://localhost:${3000}/deals/${id}`, "PUT", JSON.stringify(body));
-    }
+	const editDeal = async (body, id) => {
+		return await httpRequest(
+			`${HOST}:${PORT}/deals/${id}`,
+			"PUT",
+			JSON.stringify(body)
+		)
+	}
 
-    const deleteDeal = async (id) => {
-        return await httpRequest(`http://localhost:${3000}/deals/${id}`, "DELETE");
-    }
+	const deleteDeal = async (id) => {
+		return await httpRequest(`${HOST}:${PORT}/deals/${id}`, "DELETE")
+	}
 
-    return {
-        loading, error, 
+	return {
+		loading,
+		error,
 
-        getEmployee, 
-        editEmployee,
+		getEmployee,
+		editEmployee,
 
-        getEmployees, 
-        setEmployees, 
-        deleteEmployees, 
+		getEmployees,
+		setEmployees,
+		deleteEmployees,
 
-        getDeals, 
+		getDeals,
 
-        getDeal, 
-        setDeal, 
-        editDeal,
-        deleteDeal, 
-    };
-
+		getDeal,
+		setDeal,
+		editDeal,
+		deleteDeal,
+	}
 }
 
-export default useData;
+export default useData
